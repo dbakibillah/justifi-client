2d60d45992c7116b7ec9e6670e9e96c0ca70677a
tushar123 <asiif32ahamed@gmail.com>
2025-08-30 00:59:22 +0600

    footer

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../common/navbar/Navbar";
+import Footer from "../common/footer/Footer";
 
 const Root = () => {
     return (
         <section>
             <Navbar />
             <Outlet />
-            footer
+            <Footer/>
         </section>
     );
 };
