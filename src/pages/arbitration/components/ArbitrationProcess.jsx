ca62b1b6e727c920d55ef050abcce4841960ed2d
palash1212 <nahidurrahman927@gmail.com>
2025-10-10 05:27:34 +0600

    arbitration link add to begin your case button

--- src/pages/arbitration/components/ArbitrationProcess.jsx
+++ src/pages/arbitration/components/ArbitrationProcess.jsx
@@ -7,6 +7,7 @@ import {
   FaVideo,
   FaGavel,
 } from "react-icons/fa";
+import { Link } from "react-router";
 
 function ArbitrationProcess() {
   const steps = [
@@ -100,9 +101,11 @@ function ArbitrationProcess() {
             efficient, professional dispute resolution.
           </p>
           <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
-            <button className="bg-white text-blue-800 px-8 py-3 rounded-lg font-medium hover:bg-blue-100 transition">
-              Begin Your Case
-            </button>
+            <Link to={`/arbitration`}>
+              <button className="bg-white text-blue-800 px-8 py-3 rounded-lg font-medium hover:bg-blue-100 transition">
+                Begin Your Case
+              </button>
+            </Link>
           </div>
         </div>
       </section>
